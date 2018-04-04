370e9f29d17ea302d173388be10e517f8d3a288c
kckrishna25 <krishna.k.kc@gmail.com>
2018-04-03 23:03:36 -0500

    Second commit

--- app/src/main/java/com/testapp/krish/firstapp/Day0/SplashActivity.java
+++ app/src/main/java/com/testapp/krish/firstapp/Day0/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.testapp.krish.firstapp.Day0;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,21 +13,25 @@ import com.testapp.krish.firstapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, BaseActivity.class);
+                startActivity(intent);
+
+                finish();
             }
-        });
+        },SPLASH_TIME_OUT);
+
+
+
     }
 
 }
