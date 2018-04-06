f7e56fa2d9c60357d9d62b77153f91188abed7a4
kckrishna25 <krishna.k.kc@gmail.com>
2018-04-05 21:19:34 -0500

    Commit till Assignment 3

--- app/src/main/java/com/testapp/krish/firstapp/Day0/BaseActivity.java
+++ app/src/main/java/com/testapp/krish/firstapp/Day0/BaseActivity.java
@@ -9,11 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 
 import com.testapp.krish.firstapp.Day1.MainActivity;
 import com.testapp.krish.firstapp.Day2.ConverterActivity;
 import com.testapp.krish.firstapp.Day2.DinnerDeciderActivity;
+import com.testapp.krish.firstapp.Day3.ImplicitIntentActivity;
+import com.testapp.krish.firstapp.Day3.WalmartStoreActivity;
+import com.testapp.krish.firstapp.Day4.AutoCompleteTextViewDemo;
 import com.testapp.krish.firstapp.R;
 
 public class BaseActivity extends ListActivity {
@@ -23,7 +27,7 @@ public class BaseActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
 
-        String[] menu = {"Main Activity", "Converter Activity", "Dinner Selection"};
+        String[] menu = {"Main Activity", "Converter Activity", "Dinner Selection", "Walmart Store","AutoCompleteTextViewDemo", "ImplicitIntentActivity"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(BaseActivity.this, android.R.layout.simple_list_item_1,menu);
         setListAdapter(adapter);
 
@@ -41,6 +45,15 @@ public class BaseActivity extends ListActivity {
         }
         else if(position == 2){
             startActivity(new Intent(BaseActivity.this, DinnerDeciderActivity.class));
+        }
+        else if(position == 3){
+            startActivity(new Intent(BaseActivity.this, WalmartStoreActivity.class));
+        }
+        else if(position == 4){
+            startActivity(new Intent(BaseActivity.this, AutoCompleteTextViewDemo.class));
+        }
+        else if(position == 5){
+            startActivity(new Intent(BaseActivity.this, ImplicitIntentActivity.class));
         }
     }
 }
---
+++ app/src/main/java/com/testapp/krish/firstapp/Day3/WalmartStoreActivity.java
@@ -0,0 +1,92 @@
+package com.testapp.krish.firstapp.Day3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.testapp.krish.firstapp.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WalmartStoreActivity extends AppCompatActivity {
+
+    static ArrayList<User> users;
+    EditText userName, password;
+    Button btn;
+    TextView text_createaccount;
+
+    String userNameStr, passwordStr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walmart_store);
+        userName = (EditText) findViewById(R.id.editText_UserName);
+        password = (EditText) findViewById(R.id.editText_password);
+
+
+        users = new ArrayList<User>(Arrays.asList(
+                new User("Krishna", "KC", "kkc@mum.edu","kkc"),
+                new User("Niwesh", "Rai", "ncrai@mum.edu", "ncrai")
+        ));
+
+
+
+        btn = (Button) findViewById(R.id.btn_signin);
+        text_createaccount = (TextView) findViewById(R.id.txt_register);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                userNameStr = userName.getText().toString();
+                Toast.makeText(WalmartStoreActivity.this, userNameStr, Toast.LENGTH_SHORT).show();
+                passwordStr = password.getText().toString();
+                Toast.makeText(WalmartStoreActivity.this, passwordStr, Toast.LENGTH_SHORT).show();
+
+                for(User u : users){
+                    if(u.geteMail().equals(userNameStr) && u.getPassword().equals(passwordStr)){
+                        Intent intent = new Intent(WalmartStoreActivity.this, ShoppingActivity.class);
+                        intent.putExtra("userNameInput",userNameStr);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(WalmartStoreActivity.this, "Username or Password did not match !!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+
+//        text_createaccount.setOnTouchListener(new View.OnTouchListener(){
+//
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                Intent intent = new Intent(WalmartStoreActivity.this, CreateAccountActivity.class);
+//                startActivity(intent);
+//
+//                return true;
+//            }
+//        });
+
+
+
+
+    }
+    public void registerClick(View v){
+        Intent intent = new Intent(WalmartStoreActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+}
